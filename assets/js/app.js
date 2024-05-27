6b06eb237981cc235f65a46d612325ecd71e418c
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-27 10:42:15 +0200

    Added request location functionality and modified the live tracking functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,68 @@
+const latitudeEl = document.querySelector("#lat");
+const longitudeEl = document.querySelector("#long");
+const accuracyEl = document.querySelector("#acc");
+const feedbackEl = document.querySelector("#feedback");
+
+let locationWatch;
+let marker;
+let circle;
+let zoomed;
+
+document.querySelector("#btn-request").addEventListener("click", getGeolocation);
+document.querySelector("#btn-watch").addEventListener("click", watchGeolocation);
+document.querySelector("#btn-stop").addEventListener("click", stopWatchGeolocation);
+
+function getGeolocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    feedbackEl.textContent = "let\'s see where you are, just this once...";
+  } else {
+    feedbackEl.textContent = "Geolocation is not supported by this browser.";
+  }
+}
+
+function watchGeolocation() {
+  if (navigator.geolocation) {
+    locationWatch = navigator.geolocation.watchPosition(success, error);
+    console.log("started location watcher");
+    feedbackEl.textContent = "Following your location...";
+  } else {
+    feedbackEl.textContent = "Geolocation is not supported by this browser.";
+  }
+}
+
+function stopWatchGeolocation() {
+  if (navigator.geolocation) {
+    //stop watcher stored in locationWatch
+    navigator.geolocation.clearWatch(locationWatch);
+    console.log("cleared location watcher");
+    feedbackEl.textContent = "No more following your location...";
+  }
+
+}
+function showPosition(pos) {
+  const lat = pos.coords.latitude;
+  const lng = pos.coords.longitude;
+  const accuracy = pos.coords.accuracy;
+
+  if (marker && circle) {
+    map.removeLayer(marker);
+    map.removeLayer(circle);
+  }
+
+  marker = L.marker([lat, lng]).addTo(map);
+  circle = L.circle([lat, lng], {radius: accuracy}).addTo(map);
+
+  zoomed = map.fitBounds(circle.getBounds());
+
+  map.setView([lat, lng]);
+
+  latitudeEl.innerText = pos.coords.latitude;//always returned
+  longitudeEl.innerText = pos.coords.longitude;//always returned
+  accuracyEl.innerText = pos.coords.accuracy;
+  console.log(pos);
+}
+
 var map = L.map('map').setView([50.850346, 4.351721], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -39,33 +104,30 @@ var castleKevergem = L.circle([51.177403, 3.23568], {
   radius: 250
 }).addTo(map);
 
-// navigator.geolocation.watchPosition(success, error);
+function success(pos) {
+  const lat = pos.coords.latitude;
+  const lng = pos.coords.longitude;
+  const accuracy = pos.coords.accuracy;
 
-let marker;
-let circle;
-let zoomed;
+  if (marker) {
+    map.removeLayer(marker);
+    map.removeLayer(circle);
+  }
 
-// function success(pos) {
-//   const lat = pos.coords.latitude;
-//   const lng = pos.coords.longitude;
-//   const accuracy = pos.coords.accuracy;
+  marker = L.marker([lat, lng]).addTo(map);
+  circle = L.circle([lat, lng], {radius: accuracy}).addTo(map);
 
-//   if (marker) {
-//     map.removeLayer(marker);
-//     map.removeLayer(circle);
-//   }
+  if (!zoomed) {
+    zoomed = map.fitBounds(circle.getBounds());
+  }
 
-//   marker = L.marker([lat, lng]).addTo(map);
-//   circle = L.circle([lat, lng], {radius: accuracy}).addTo(map);
+  map.setView([lat, lng]);
 
-//   if (!zoomed) {
-//     zoomed = map.fitBounds(circle.getBounds());
-//   }
-
-//   map.setView([lat, lng]);
-
-//   console.log(pos);
-// };
+  latitudeEl.innerText = pos.coords.latitude;//always returned
+  longitudeEl.innerText = pos.coords.longitude;//always returned
+  accuracyEl.innerText = pos.coords.accuracy;
+  console.log(pos);
+};
 
 function error(err) {
  if (err.code === 1) {
@@ -83,6 +145,7 @@ var polygon = L.polygon([
 
 // marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 // circle.bindPopup("I am a circle.");
+castleRooigem.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
 var popup = L.popup();
 
